9e86e0590dc34dfd82e38d40ddbacda28c7f5c63
MichaelKostroma <mikbingo2018@gmail.com>
2022-02-11 10:18:16 +0200

    fresh data update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import './App.css';
 import {useState, useEffect} from "react";
 import axios from "axios";
 import {BrowserRouter as Router} from "react-router-dom";
 import Routes from "./Routes";
+import './App.css';
+
 
 function App() {
 
   const [rates, setRates] = useState([]);
 
   useEffect(() => {
-    axios.get('http://data.fixer.io/api/latest?access_key=332109aae8b978dd8ba64d60634d0e9f')
-        .then(response => {
-          setRates(response.data.rates);
-        })
+
+      axios.get('http://data.fixer.io/api/latest?access_key=332109aae8b978dd8ba64d60634d0e9f')
+          .then(response => {
+              setRates(response.data.rates);
+          })
+
+      const FreshDataFetch = setInterval(() => {
+          axios.get('http://data.fixer.io/api/latest?access_key=332109aae8b978dd8ba64d60634d0e9f')
+              .then(response => {
+                  setRates(response.data.rates);
+              })
+      }, 1800000);
+
+      return () => FreshDataFetch
   }, []);
 
   function format(number) {
